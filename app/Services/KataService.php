dff4f9d5bb26d3c3ea801c08cf2fd7564d1df5d0
Daniel Campos <dcampos2015@hotmail.com>
2020-10-29 04:42:45 -0700

    aplico PSR-2 a clase

--- app/Services/KataService.php
+++ app/Services/KataService.php
@@ -2,14 +2,16 @@
 
 namespace App\Services;
 
-class KataService {
+class KataService
+{
 
     private $countHyphenat = 0;
     private $countSpace = 0;
     private $count = 0;
     private $birdName = '';
 
-    public function __construct(int $countHyphenat, int $countSpace, string $birdName){
+    public function __construct(int $countHyphenat, int $countSpace, string $birdName)
+    {
         $this->countHyphenat = $countHyphenat;
         $this->countSpace = $countSpace;
         $this->count = $countHyphenat + $countSpace;
@@ -18,13 +20,13 @@ class KataService {
 
     public function generateKata()
     {
-        if($this->count == 0){
+        if ($this->count == 0) {
             $kata = $this->onePhrase();
         } else {
-            if($this->count == 1) {
+            if ($this->count == 1) {
                 $kata = $this->twoPhrases();
             } else {
-                if($this->count == 2) {
+                if ($this->count == 2) {
                     $kata = $this->threePhrases();
                 } else {
                     $kata = $this->fourPhrases();
@@ -44,7 +46,7 @@ class KataService {
     {
         $kata = '';
         $twoPhrasesArrays = str_word_count($this->birdName, 1);
-        foreach($twoPhrasesArrays as $phrase){
+        foreach ($twoPhrasesArrays as $phrase) {
             $kata .= substr($phrase, 0, 2);
         }
         return $kata;
@@ -54,9 +56,9 @@ class KataService {
     {
         $kata = '';
         $threePhrasesArrays = str_word_count($this->birdName, 1);
-        foreach($threePhrasesArrays as $phrase){
-            if(substr_count($phrase, "-") == 0){
-                if($phrase !== end($threePhrasesArrays) && $this->countSpace == $this->count) {
+        foreach ($threePhrasesArrays as $phrase) {
+            if (substr_count($phrase, "-") == 0) {
+                if ($phrase !== end($threePhrasesArrays) && $this->countSpace == $this->count) {
                     $kata .= substr($phrase, 0, 1);
                 } else {
                     $kata .= substr($phrase, 0, 2);
@@ -64,8 +66,8 @@ class KataService {
             } else {
                 $twoPhrases = str_replace("-", " ", $phrase);
                 $twoPhrasesArrays = str_word_count($twoPhrases, 1);
-                foreach($twoPhrasesArrays as $phrase){
-                    if($phrase === end($twoPhrasesArrays) && $this->countHyphenat == $this->count){
+                foreach ($twoPhrasesArrays as $phrase) {
+                    if ($phrase === end($twoPhrasesArrays) && $this->countHyphenat == $this->count) {
                         $kata .= substr($phrase, 0, 2);
                     } else {
                         $kata .= substr($phrase, 0, 1);
@@ -81,7 +83,7 @@ class KataService {
         $kata = '';
         $fourPhrases = str_replace("-", " ", $this->birdName);
         $fourPhrasesArrays = str_word_count($fourPhrases, 1);
-        foreach($fourPhrasesArrays as $phrase){
+        foreach ($fourPhrasesArrays as $phrase) {
             $kata .= substr($phrase, 0, 1);
         }
         return $kata;
